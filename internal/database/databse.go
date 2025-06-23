a0ea5054a7f67b7209c2845d8da5ad392f24a9bb
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2025-06-23 18:13:54 +0530

    Add functions to open database conntection and do status check.

---
+++ internal/database/databse.go
@@ -0,0 +1,66 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"net/url"
+	"time"
+
+	"github.com/iamNilotpal/k8s-demo/internal/config"
+	_ "github.com/lib/pq"
+)
+
+func Open(cfg *config.DB) (*sql.DB, error) {
+	q := url.Values{}
+	q.Set("sslmode", cfg.TLS)
+
+	u := url.URL{
+		Host:     cfg.Host,
+		Path:     cfg.Name,
+		RawQuery: q.Encode(),
+		Scheme:   cfg.Scheme,
+		User:     url.UserPassword(cfg.User, cfg.Password),
+	}
+
+	db, err := sql.Open("postgres", u.String())
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+
+	return db, nil
+}
+
+// StatusCheck returns nil if it can successfully talk to the database. It
+// returns a non-nil error otherwise.
+func StatusCheck(ctx context.Context, db *sql.DB) error {
+	// If the user doesn't give us a deadline set 10 seconds.
+	if _, ok := ctx.Deadline(); !ok {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, time.Second*10)
+		defer cancel()
+	}
+
+	for attempts := 1; ; attempts++ {
+		if err := db.Ping(); err == nil {
+			break
+		}
+
+		time.Sleep(time.Duration(attempts) * 200 * time.Millisecond)
+
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+	}
+
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+
+	// Run a simple query to determine connectivity.
+	const q = `SELECT TRUE`
+	var tmp bool
+	return db.QueryRowContext(ctx, q).Scan(&tmp)
+}
